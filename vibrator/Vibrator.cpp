630fd951a3d01d6e63f75f58fa93640efc920dfe
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-05 15:18:35 +0300

    ruby: vibrator: add support for aw8697_haptic input device
    Change-Id: Icdf8a884968ff9abe3e697c82fc22262d9130012

--- vibrator/Vibrator.cpp
+++ vibrator/Vibrator.cpp
@@ -111,7 +111,8 @@ InputFFDevice::InputFFDevice()
             continue;
         }
 
-        if (strcmp(name, "qcom-hv-haptics") && strcmp(name, "qti-haptics")) {
+        if (strcmp(name, "qcom-hv-haptics") && strcmp(name, "qti-haptics")
+            && strcmp(name, "aw8697_haptic")) {
             ALOGD("not a qcom/qti haptics device\n");
             close(fd);
             continue;
