f9f9ac0613e1321ec66d1c542bac213f82706960
bengris32 <bengris32@protonmail.ch>
2023-07-01 18:06:54 +0700

    ruby: Initial minimised rootdir
    Change-Id: I94815f423f5f2f6dc1c41138428b274df1ce675f
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -10,6 +10,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    fstab.mt6877 \
+    fstab.mt6877.ramdisk \
+    init.batterysecret.rc \
+    init.cgroup.rc \
+    init.connectivity.rc \
+    init.mi_thermald.rc \
+    init.modem.rc \
+    init.mt6877.rc \
+    init.mt6877.usb.rc \
+    init.project.rc \
+    init.sensor_2_0.rc \
+    init.stnfc.rc \
+    ueventd.mt6877.rc
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 31
 
