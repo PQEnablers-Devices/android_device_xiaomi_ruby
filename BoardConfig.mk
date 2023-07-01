1bcd58d1d9db72feec9529d6fb74674ee7052338
bengris32 <bengris32@protonmail.ch>
2023-07-01 16:41:34 +0700

    ruby: Define Architecture flags
    Change-Id: I14b27a4ab779d367371e481a8ef79b8bf406568b
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,20 @@
 
 DEVICE_PATH := device/xiaomi/ruby
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a78
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
+
 # Inherit the proprietary files
 include vendor/xiaomi/ruby/BoardConfigVendor.mk
