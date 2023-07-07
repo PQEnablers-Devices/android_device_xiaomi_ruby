44792837e6fde81f0209affbef1b77ec4fa7ee2f
bengris32 <bengris32@protonmail.ch>
2023-07-07 22:56:23 +0700

    ruby: Enable support for EXT4 & F2FS formatted userdata
    Change-Id: I88ce22a250e1f7e5882cb225cca8691af587985a
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,6 +88,8 @@ BOARD_HAS_MTK_HARDWARE := true
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.mt6877
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
