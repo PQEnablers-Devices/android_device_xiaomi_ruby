d254283f7bdf88595731b5cc937af29088f37ed4
bengris32 <bengris32@protonmail.ch>
2023-07-18 19:47:40 +0100

    ruby: Include `hardware/mediatek` Soong namespace
    Change-Id: I9028cec415568445e4bf6b2cb7cdf3f03e95154d
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -70,7 +70,8 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/mediatek
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/ruby/ruby-vendor.mk)
