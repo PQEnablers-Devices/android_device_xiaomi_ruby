1cba75df74c2c470a26ace233b034bebab6478a1
bengris32 <bengris32@protonmail.ch>
2023-07-19 14:48:34 +0100

    ruby: Configure Media stack
    * Reference: https://github.com/moto-common/android_device_mediatek_common/blob/master/vendor/media-c2/proprietary-files.txt
    
    Change-Id: I63d5365f4c52ef8f366c66f45cc6f4210c705a8c
    Co-authored-by: electimon <electimon@gmail.com>
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -115,6 +115,23 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor \
     android.hardware.hardware_keystore.km41.xml
 
+# Media
+PRODUCT_PACKAGES += \
+    libcodec2_hidl@1.1.vendor \
+    libcodec2_hidl@1.2.vendor \
+    libavservices_minijail_vendor \
+    libstagefright_softomx_plugin.vendor \
+    libsfplugin_ccodec_utils.vendor \
+    libcodec2_soft_common.vendor
+
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/seccomp,$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy) \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
