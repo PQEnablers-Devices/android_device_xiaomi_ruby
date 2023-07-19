ca6ce1fef4f1340ef6cced353f99535a88675fb5
Bruno Martins <bgcngm@gmail.com>
2023-07-19 20:16:11 +0100

    ruby: Build AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- device.mk
+++ device.mk
@@ -93,6 +93,10 @@ PRODUCT_PACKAGES += \
     libdrm.vendor \
     libutils-v32
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm-service.clearkey
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
