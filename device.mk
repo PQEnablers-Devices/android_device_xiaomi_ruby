816299619623eb68c3e13eb4d2f5161693691a81
bengris32 <bengris32@protonmail.ch>
2023-07-19 13:18:58 +0100

    ruby: Configure Bluetooth stack
    * Reference: https://github.com/moto-common/android_device_mediatek_common/blob/master/vendor/bt/proprietary-files.txt
    
    Change-Id: I2fc37ada6618ba89bbeee83b5e17c2754a53d82f
    Co-authored-by: electimon <electimon@gmail.com>
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -74,6 +74,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.1.vendor
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
