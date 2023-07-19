9da166a892581b70f7ec8e36c6ec223aea3f8f11
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-07-19 10:50:14 +0100

    ruby: Build default health HIDL
    Change-Id: I72bf9b21274d27952997b9ddcb3f1ae1a7ad8013

--- device.mk
+++ device.mk
@@ -54,6 +54,12 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
