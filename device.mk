da3af6693dd36f64bbac6bd0e594b9f4addb9c82
Lokesh Gidra <lokeshgidra@google.com>
2023-07-19 20:19:00 +0100

    ruby: Inherit the Dalvik heap configuration for a 6GB device
     * The Redmi Note 12 Pro series has multiple variants with
       varying RAM capacities but it's not worth making custom
       values for each variant, can end up doing more harm than good.
    
     * For now lets just use the verified ones from AOSP for the
       highest capacity variant they have.
    
    Change-Id: Ib6b1d7ad933fe3867dc6253e229a3c6470135e75

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Dalvik VM Configuration
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Inherit virtual_ab_ota product
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression_retrofit.mk)
