fdafa7934f79ce9215e91f156ad86b97a9a0c942
bengris32 <bengris32@protonmail.ch>
2023-07-19 16:08:20 +0100

    ruby: Configure ConsumerIR stack
    Change-Id: I4142bab90cacbd9f92fda53dc32dbba4927edc2f
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -78,6 +78,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor
 
+# ConsumerIr
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
