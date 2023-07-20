8d70b481d35e539538b0173b2cfe6ebaaedac946
bengris32 <bengris32@protonmail.ch>
2023-07-20 15:37:08 +0100

    ruby: Configure Fingerprint stack
    Change-Id: I50d56b6eaea6fa763067f8ae6ea99b6426278f9e
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -107,6 +107,11 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    vendor.xiaomi.hardware.fingerprintextension@1.0.vendor
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
