99ba183e0e755359fb2bf668ecfe8cdefeaf7d13
Davide Garberi <dade.garberi@gmail.com>
2023-07-20 14:46:08 +0100

    ruby: Build `libhidltransport` and `libhwbinder`
    Change-Id: Id87af3af701000734aaa89f1ec98a80dd70a266b

--- device.mk
+++ device.mk
@@ -118,6 +118,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport.vendor \
+    libhwbinder.vendor
+
 # Keymaster
 PRODUCT_PACKAGES += \
     libkeymaster4_1support.vendor \
