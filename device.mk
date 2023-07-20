3bf7b31611ab1146f605371790d9499a70af4dd8
bengris32 <bengris32@protonmail.ch>
2023-07-20 14:27:49 +0100

    ruby: Configure Sensors stack
    * Also uprev sensors HIDL to 2.1 while we're at it.
    
    Change-Id: I8d3e5c1ea0f72290c7c814167ddebf20d1cb4f2d
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -226,6 +226,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.recovery.mt6877.rc
 
+# Sensors
+PRODUCT_PACKAGES += \
+    libsensorndkbridge \
+    android.hardware.sensors@1.0.vendor \
+    android.hardware.sensors@2.1.vendor \
+    android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.sensors@2.1-service.multihal
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 31
 
