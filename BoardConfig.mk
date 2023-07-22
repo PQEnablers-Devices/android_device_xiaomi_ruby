b4b6abae3cbcc357d5c6de3cdfced88715cb3293
David Sehr <sehr@google.com>
2023-07-22 17:06:45 +0100

    ruby: Use dex2oat64 for better dexopt time
    Enable dex2oat64 usage on ruby.
    
    Bug: 153380900
    Test: boot and install an application.
    Change-Id: I101ebfe5865da84baa90c55d2246dbd4287a9b72

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,6 +113,7 @@ TARGET_BOARD_PLATFORM := mt6877
 BOARD_HAS_MTK_HARDWARE := true
 
 # Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
