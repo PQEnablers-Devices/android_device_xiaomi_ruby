32c01723d31c7803bc7479d1d4e5c2cca740afd9
bengris32 <bengris32@protonmail.ch>
2023-07-24 11:01:11 +0100

    ruby: Configure GNSS stack
    * Reference: https://github.com/moto-common/android_device_mediatek_common/tree/master/vendor/gnss
    
    Change-Id: I598002e24d64f10a7d7ea8483a1615d925f54e39
    Co-authored-by: electimon <electimon@gmail.com>
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -128,6 +128,14 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
+# GNSS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss.measurement_corrections@1.1.vendor \
+    android.hardware.gnss.visibility_control@1.0.vendor \
+    android.hardware.gnss@1.1.vendor \
+    android.hardware.gnss@2.1.vendor \
+    android.hardware.gnss-V1-ndk.vendor
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
