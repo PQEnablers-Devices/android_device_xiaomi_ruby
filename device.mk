146636ab9166de282e8fb0bf378662cfab219c94
bengris32 <bengris32@protonmail.ch>
2023-07-24 10:22:51 +0100

    ruby: Configure vendor logtags
    Change-Id: I83b1e3d5691c7c22a5fef5f03ce3172ac47298d3
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -259,6 +259,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/perf,$(TARGET_COPY_OUT_VENDOR)/etc)
 
+# Properties
+include $(LOCAL_PATH)/vendor_logtag.mk
+
 # Radio
 PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.3.vendor \
