e1ec87c6f40fae5f53ce1ee450713ee100503c77
SamarV-121 <samarvispute121@gmail.com>
2023-07-24 19:54:51 +0100

    ruby: Import HotwordEnrollment blobs
    Change-Id: I76c1f0a4973ab82f51354075cd284c5ab4265374

--- device.mk
+++ device.mk
@@ -269,6 +269,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
+    $(LOCAL_PATH)/configs/com.android.hotwordenrollment.common.util.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.android.hotwordenrollment.common.util.xml
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-mediatek \
