53fdbc45924760e6dc210dd20d06e2daba527988
bengris32 <bengris32@protonmail.ch>
2023-07-24 18:27:29 +0100

    ruby: Configure MediaTek IMS stack
    * Import required system IMS blobs from MIUI V14.0.3.0.TMOEUXM.
    * Set the default IMS implementation to com.mediatek.ims via
      overlay.
    * Force enable IMS features via overlay and sysprops.
    * Add MediaTek framework jars to bootjars.
    * Copy priv-app permissions for MediaTek IMS.
    * Pull missing vendor videotelephony implementation.
    
    Change-Id: Ie258bff336586919a1127c3f7241e3ef9d689dd5
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -148,6 +148,19 @@ PRODUCT_PACKAGES += \
     libhwbinder.vendor \
     android.hidl.allocator@1.0.vendor
 
+# IMS
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
+
 # Keymaster
 PRODUCT_PACKAGES += \
     libkeymaster4_1support.vendor \
@@ -202,6 +215,7 @@ PRODUCT_PACKAGES += \
     FrameworksResOverlayRubyPro \
     SettingsOverlayRuby \
     SystemUIOverlayRuby \
+    TelephonyOverlayRuby \
     WifiResOverlayRuby
 
 DEVICE_PACKAGE_OVERLAYS += \
