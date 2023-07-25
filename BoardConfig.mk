c1d5c11fdf8d2dc4ef04e5f053c5255e898cef14
Arian <arian.kulmer@web.de>
2023-07-25 21:23:35 +0100

    ruby: Add power HAL extension and use xiaomi-touch to handle dt2w
    Change-Id: I5c65e0df255853eb2b3018a27ceb807c111bc4df

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -112,6 +112,9 @@ TARGET_COPY_OUT_VENDOR := vendor
 TARGET_BOARD_PLATFORM := mt6877
 BOARD_HAS_MTK_HARDWARE := true
 
+# Power
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
