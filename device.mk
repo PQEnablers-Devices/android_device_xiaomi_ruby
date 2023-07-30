5a12f0e1f559998c13cd252366d3c5f499b4c1b4
bengris32 <bengris32@protonmail.ch>
2023-07-30 11:51:10 +0100

    ruby: Fallback to HIDL Clearkey HAL
    Change-Id: Ide5cd400b8261ecbddaf55ef6ef6b0a33e59c2d2
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -112,7 +112,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor
