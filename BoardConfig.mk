47be712b8280c4fa651703b1a2dc71042c29bc4c
Aayush Gupta <aayushgupta219@gmail.com>
2023-08-05 12:34:33 +0100

    ruby: Inherit sepolicy for mediatek devices
    There are lot of denials which are not device-specific. Henceforth,
    move such denials to a seperate sepolicy repository and inherit from it.
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I95c1f35006d49b797c617c5fea5ff27e5f98d45e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -144,6 +144,9 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
 
+# SELinux
+include device/mediatek/sepolicy_vndr/SEPolicy.mk
+
 # SPL
 VENDOR_SECURITY_PATCH := 2023-06-01
 
