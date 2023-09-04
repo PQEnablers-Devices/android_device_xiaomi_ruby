beebc8960a80468fb7732034e55a3f35fbb6f4f7
bengris32 <bengris32@protonmail.ch>
2023-09-04 19:59:12 +0100

    ruby: Switch to AIDL ConsumerIr service
    Change-Id: I1c0fbe5237d344c0a2891799d8189b347f43ea02
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -91,8 +91,7 @@ PRODUCT_PACKAGES += \
 
 # ConsumerIr
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
+    android.hardware.ir-service.mediatek
 
 # Cgroup
 PRODUCT_COPY_FILES += \
