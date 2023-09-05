73993912e20bb9e0c8451931362e8f35ff9215f6
Josh Wu <joshwu@google.com>
2023-09-05 12:32:13 +0100

    ruby: Switch BtAudio to AIDL
    Test: m
    Bug: 203490261
    
    Change-Id: I80eced28790a15ad5485c9780ccf5812ab75dc1d

--- device.mk
+++ device.mk
@@ -59,7 +59,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
-    android.hardware.bluetooth.audio@2.1-impl
+    android.hardware.bluetooth.audio-impl
 
 PRODUCT_PACKAGES += \
     MtkInCallService
