e400d7ee597a386c627280707337654b15707e7d
bengris32 <bengris32@protonmail.ch>
2023-09-05 12:37:49 +0100

    ruby: Switch to AOSP audio service
    Change-Id: I933b66e3821562022eb837af91ad4039e22fb578
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -55,7 +55,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.soundtrigger@2.3-impl:32 \
-    android.hardware.audio.service.mediatek
+    android.hardware.audio.service
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
