b43174b3d51a3768b540ad262d89e09d5f37ff02
bengris32 <bengris32@protonmail.ch>
2023-09-11 15:18:14 +0100

    ruby: Import stripped down libinit from sm8250-common
    * Everything removed apart from libinit_variant without NFC
      variant handling.
    
    Change-Id: I1ebdfef22a4b1547c2071748af26be03552dd980
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

---
+++ libinit/init_xiaomi_ruby.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <libinit_variant.h>
+#include <libinit_utils.h>
+
+#include "vendor_init.h"
+
+void vendor_load_properties() {
+    search_variant(variants);
+}
