86bfd4680159136a7b09c5fe18791126f698c57b
bengris32 <bengris32@protonmail.ch>
2023-09-30 23:12:13 +0100

    ruby: libinit: Set `ro.product.mod_device` property
    Change-Id: I85baa2a62d79526737b5d65aa93f9042262ae6a0
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- libinit/init_xiaomi_ruby.cpp
+++ libinit/init_xiaomi_ruby.cpp
@@ -16,6 +16,7 @@ static const variant_info_t ruby_global_info = {
 
     .brand = "Redmi",
     .device = "ruby",
+    .mod_device = "ruby_global",
     .marketname = "Redmi Note 12 Pro 5G",
     .model = "22101316G",
     .build_fingerprint = "Redmi/ruby_global/ruby:12/SP1A.210812.016/V14.0.1.0.TMOMIXM:user/release-keys"
@@ -27,6 +28,7 @@ static const variant_info_t rubypro_global_info = {
 
     .brand = "Redmi",
     .device = "rubypro",
+    .mod_device = "ruby_global",
     .marketname = "Redmi Note 12 Pro+ 5G",
     .model = "22101316UG",
     .build_fingerprint = "Redmi/rubypro_global/rubypro:12/SP1A.210812.016/V14.0.1.0.TMOMIXM:user/release-keys"
@@ -38,6 +40,7 @@ static const variant_info_t rubyplus_info = {
 
     .brand = "Redmi",
     .device = "rubyplus",
+    .mod_device = "ruby",
     .marketname = "Redmi Note 12 DISCOVERY EDITION",
     .model = "22101316UC",
     .build_fingerprint = "Redmi/rubyplus/rubyplus:12/SP1A.210812.016/V14.0.3.0.TMOCNXM:user/release-keys"
--- libinit/libinit_variant.cpp
+++ libinit/libinit_variant.cpp
@@ -34,6 +34,8 @@ void set_variant_props(const variant_info_t variant) {
     set_ro_build_prop("marketname", variant.marketname, true);
     set_ro_build_prop("model", variant.model, true);
 
+    property_override("ro.product.mod_device", variant.mod_device, true);
+
     property_override("vendor.usb.product_string", variant.marketname, true);
     property_override("bluetooth.device.default_name", variant.marketname, true);
     set_ro_build_prop("fingerprint", variant.build_fingerprint);
