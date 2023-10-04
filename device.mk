317c17908fea2bd3c5ed7d8b3ce9fffc5a22c109
bengris32 <bengris32@protonmail.ch>
2023-10-05 00:14:52 +0100

    ruby: Switch Boot HAL to AIDL
    Change-Id: Ic921cb8e3f5a70665f09c9506e5ab8311daaa904
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -22,9 +22,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # A/B
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
-    android.hardware.boot@1.2-service
+    android.hardware.boot-service.default \
+    android.hardware.boot-service.default_recovery
 
 PRODUCT_PACKAGES += \
     update_engine \
