31b8bee990a3d4b80031b8c7f20d0d2b67273584
bengris32 <bengris32@protonmail.ch>
2023-10-05 15:34:45 +0100

    ruby: Switch Thermal to AIDL
    Change-Id: I89be258d0683e3497639551a76459527d4366afa
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -358,7 +358,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.mediatek
+    android.hardware.thermal-service.mediatek
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
