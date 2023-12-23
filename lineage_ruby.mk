1af2f66d5f43549d92393826a7827343ad38dda1
bkgrg <71226962+bkgrg@users.noreply.github.com>
2023-12-24 00:19:43 +0530

    Update lineage_ruby.mk

--- lineage_ruby.mk
+++ lineage_ruby.mk
@@ -12,6 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/ruby/device.mk)
 
 # Inherit some common LineageOS stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_ruby
@@ -20,7 +22,9 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 22101316G
 
+# GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+WITH_GMS := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="ruby-user 12 SP1A.210812.016 V14.0.1.0.TMOMIXM release-keys"
